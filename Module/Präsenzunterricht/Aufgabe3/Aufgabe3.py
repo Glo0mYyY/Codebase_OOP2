138929eb16c7bea65ab90a071923971573bf0858
Jan Niederhauser <Jan.Niederhauser@hftm.ch>
2024-08-13 18:17:13 +0200

    current directory changed to script location

--- Module/Präsenzunterricht/Aufgabe3/Aufgabe3.py
+++ Module/Präsenzunterricht/Aufgabe3/Aufgabe3.py
@@ -1,6 +1,8 @@
 import file_operations
+import os
 
-
+# Change current working directory to the directory of the script
+os.chdir(os.path.dirname(os.path.abspath(__file__)))
 
 file_operations.write_file('test.txt', 'Hello World')
 print(file_operations.read_file('test.txt'))
