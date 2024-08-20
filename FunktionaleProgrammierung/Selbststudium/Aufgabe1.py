aa5c91dc3c331f02c25ee7cf007d1b9d294e6908
Jan Niederhauser <Jan.Niederhauser@hftm.ch>
2024-08-20 18:07:29 +0200

    Nur gerade Zahlen hinzugefügt

--- FunktionaleProgrammierung/Selbststudium/Aufgabe1.py
+++ FunktionaleProgrammierung/Selbststudium/Aufgabe1.py
@@ -1,5 +1,6 @@
 def squared(numbers):
-    squares = map(lambda x: x**2, numbers)
+    filteredList = list (filter(lambda x: x % 2 == 0, numbers))
+    squares = map(lambda x: x**2, filteredList)
     return squares
 
 print(list(squared([1, 2, 3, 4, 5])))
